ea6dcabaa8b9f04304738412676d7c9da115d591
lidor <sweet.lidor@gmail.com>
2024-03-08 11:29:48 +0200

    bbc

--- obj/Debug/net8.0/web2.AssemblyInfo.cs
+++ obj/Debug/net8.0/web2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266c26e8c8f723e26b14635833d6113231ff9312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da0144386217e930a4e400fd7fa7ffc3d9911e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("web2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- program.cs
+++ program.cs
@@ -1,49 +1,18 @@
 ﻿using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.Json;
+using System.Reflection.Metadata.Ecma335;
 
-class Country(string name, int population, bool isUnMember, string[] cities)
-{
-    public string Name { get; set; } = name;
-    public int Population { get; set; } = population;
-    public bool IsUnMember { get; set; } = isUnMember;
-    public string[] Cities { get; set; } = cities;
-}
 
 class Program
 {
     public static void Main()
     {
-        Country[] myCountry = [
-            new Country(
-                "isreal",
-                8000000,
-                true,
-                ["tel aviv","ariel","jerusalem"]
-            ),
-            new Country(
-                "USA",
-                 331000000,
-                true,
-                ["texas","california","florida"]
-            ),
-            new Country(
-                "china",
-                1500000000,
-                true,
-                ["rural", " urban regions", "beijing"]
-            )
-        ];
-
-
         HttpListener listener = new();
         listener.Prefixes.Add("http://*:5000/");
         listener.Start();
 
 
         Console.WriteLine("Server started. Listening for requests...");
+        Console.WriteLine("Main page on http://localhost:5000/website/index.html");
 
 
         while (true)
@@ -55,32 +24,25 @@ class Program
 
             string rawPath = request.RawUrl!;
             string absPath = request.Url!.AbsolutePath;
-            string filePath = "." + absPath;
 
 
             Console.WriteLine($"Received a request with path: " + rawPath);
 
-            
+
+            string filePath = "." + absPath;
+            bool isHtml = request.AcceptTypes!.Contains("text/html");
+
+
             if (File.Exists(filePath))
             {
                 byte[] fileBytes = File.ReadAllBytes(filePath);
+                if (isHtml) { response.ContentType = "text/html; charset=utf-8"; }
                 response.OutputStream.Write(fileBytes);
             }
-            else if (request.AcceptTypes!.Contains("text/html"))
+            else if (isHtml)
             {
                 response.StatusCode = (int)HttpStatusCode.Redirect;
                 response.RedirectLocation = "/website/404.html";
-            }
-            else if (absPath == "/getVideoGames")
-            {
-                string listJson = JsonSerializer.Serialize(myCountry);
-                byte[] listBytes = Encoding.UTF8.GetBytes(listJson);
-                response.OutputStream.Write(listBytes);
-            }
-            else if (absPath == "/addVideoGame")
-            {
-                string newItem = GetBody(request);
-                myCountry = [.. myCountry, newItem];
             }
 
 
@@ -94,3 +56,4 @@ class Program
         return new StreamReader(request.InputStream).ReadToEnd();
     }
 }
+ 
