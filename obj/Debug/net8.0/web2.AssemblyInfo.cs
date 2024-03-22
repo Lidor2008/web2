ec1fc9888ef41e7533ba84c8735f95bad7e91883
lidor <sweet.lidor@gmail.com>
2024-03-22 12:35:56 +0200

    גבבגבג

--- obj/Debug/net8.0/web2.AssemblyInfo.cs
+++ obj/Debug/net8.0/web2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da0144386217e930a4e400fd7fa7ffc3d9911e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6dcabaa8b9f04304738412676d7c9da115d591")]
 [assembly: System.Reflection.AssemblyProductAttribute("web2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
